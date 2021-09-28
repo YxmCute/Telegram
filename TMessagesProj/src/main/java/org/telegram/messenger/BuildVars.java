1eadaec842b2b43b89133db9fd2f63c1dc04d6d2
LiJun <lijunyxm@163.com>
2021-09-28 21:36:40 +0800

    modify app info

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -13,39 +13,49 @@ import android.content.SharedPreferences;
 
 public class BuildVars {
 
-    public static boolean DEBUG_VERSION = false;
-    public static boolean DEBUG_PRIVATE_VERSION = false;
-    public static boolean LOGS_ENABLED = false;
-    public static boolean USE_CLOUD_STRINGS = true;
-    public static boolean CHECK_UPDATES = true;
-    public static boolean NO_SCOPED_STORAGE = true/* || Build.VERSION.SDK_INT <= 28*/;
-    public static int BUILD_VERSION = 2432;
-    public static String BUILD_VERSION_STRING = "8.1.2";
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
-    public static String SMS_HASH = isStandaloneApp() ? "w0lkcmTZkKh" : (DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT");
-    public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
+  public static boolean DEBUG_VERSION         = false;
+  public static boolean DEBUG_PRIVATE_VERSION = false;
+  public static boolean LOGS_ENABLED          = false;
+  public static boolean USE_CLOUD_STRINGS     = true;
+  public static boolean CHECK_UPDATES         = true;
+  public static boolean NO_SCOPED_STORAGE     = true/* || Build.VERSION.SDK_INT <= 28*/;
+  public static int     BUILD_VERSION         = 2432;
+  public static String  BUILD_VERSION_STRING  = "8.1.2";
+  public static int     APP_ID                = 1977517;
+  public static String  APP_HASH              = "85830566ed398d325470fa2d40ab89c5";
+  public static String  SMS_HASH              =
+      isStandaloneApp() ? "w0lkcmTZkKh" : (DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT");
+  public static String  PLAYSTORE_APP_URL     =
+      "https://play.google.com/store/apps/details?id=org.telegram.messenger";
 
-    static {
-        if (ApplicationLoader.applicationContext != null) {
-            SharedPreferences sharedPreferences = ApplicationLoader.applicationContext.getSharedPreferences("systemConfig", Context.MODE_PRIVATE);
-            LOGS_ENABLED = DEBUG_VERSION || sharedPreferences.getBoolean("logsEnabled", DEBUG_VERSION);
-        }
+  static {
+    if (ApplicationLoader.applicationContext != null) {
+      SharedPreferences sharedPreferences =
+          ApplicationLoader.applicationContext.getSharedPreferences("systemConfig",
+              Context.MODE_PRIVATE);
+      LOGS_ENABLED = DEBUG_VERSION || sharedPreferences.getBoolean("logsEnabled", DEBUG_VERSION);
     }
+  }
 
-    private static Boolean standaloneApp;
-    public static boolean isStandaloneApp() {
-        if (standaloneApp == null) {
-            standaloneApp = ApplicationLoader.applicationContext != null && "org.telegram.messenger.web".equals(ApplicationLoader.applicationContext.getPackageName());
-        }
-        return standaloneApp;
-    }
+  private static Boolean standaloneApp;
 
-    private static Boolean betaApp;
-    public static boolean isBetaApp() {
-        if (betaApp == null) {
-            betaApp = ApplicationLoader.applicationContext != null && "org.telegram.messenger.beta".equals(ApplicationLoader.applicationContext.getPackageName());
-        }
-        return betaApp;
+  public static boolean isStandaloneApp() {
+    if (standaloneApp == null) {
+      standaloneApp =
+          ApplicationLoader.applicationContext != null && "org.telegram.messenger.web".equals(
+              ApplicationLoader.applicationContext.getPackageName());
     }
+    return standaloneApp;
+  }
+
+  private static Boolean betaApp;
+
+  public static boolean isBetaApp() {
+    if (betaApp == null) {
+      betaApp =
+          ApplicationLoader.applicationContext != null && "org.telegram.messenger.beta".equals(
+              ApplicationLoader.applicationContext.getPackageName());
+    }
+    return betaApp;
+  }
 }
